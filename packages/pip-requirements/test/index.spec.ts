3777bc7270372cb6eff8819a641f3f0c94b97064
ghe <lili@snyk.io>
2021-04-21 14:56:46 +0100

    feat: improve test & config setup

--- packages/pip-requirements/test/index.spec.ts
+++ packages/pip-requirements/test/index.spec.ts
@@ -1,4 +1,5 @@
-import { pipRequirementsFix } from '../../../src/packages/pip-requirements';
+import { pipRequirementsFix } from "../src";
+
 test('Is everything ready for the development?', async () => {
   expect(pipRequirementsFix()).toBeTruthy();
 });
---
+++ packages/pipenv-pipfile/src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.pipenvPipfileFix = void 0;
+function pipenvPipfileFix() {
+    console.log('pipenv Pipfile fix');
+    return true;
+}
+exports.pipenvPipfileFix = pipenvPipfileFix;
