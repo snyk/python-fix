0b4bd6b734a54814dcf24285ff592bdcf32d86d0
ghe <lili@snyk.io>
2021-06-01 15:36:27 +0100

    feat: poetry add dependency@version support
    - Check if poetry is installed
    - Check installed version is supported
    - Test matrix setup

--- packages/poetry/test/system/check-poetry-is-installed.spec.ts
+++ packages/poetry/test/system/check-poetry-is-installed.spec.ts
@@ -3,5 +3,7 @@ import { isPoetryInstalled } from '../../src';
 test('isPoetryInstalled', async () => {
   const res = await isPoetryInstalled();
   // version depends on test matrix
+  /* eslint-disable no-console */
+  console.log(`Jest test: poetry version is ${res.version}`);
   expect(res.version).not.toBeNull();
 });
