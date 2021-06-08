6baf3ad1b7fd45c58e7e1d25652200261e7f91ba
Lili Kastilio <lili@lilianakastilio.co.uk>
2021-06-08 16:02:45 +0100

    Revert "Feat/poetry install"

--- packages/poetry/test/system/check-poetry-is-installed.spec.ts
+++ packages/poetry/test/system/check-poetry-is-installed.spec.ts
@@ -3,7 +3,5 @@ import { isPoetryInstalled } from '../../src';
 test('isPoetryInstalled', async () => {
   const res = await isPoetryInstalled();
   // version depends on test matrix
-  /* eslint-disable no-console */
-  console.log(`Jest test: poetry version is ${res.version}`);
   expect(res.version).not.toBeNull();
 });
