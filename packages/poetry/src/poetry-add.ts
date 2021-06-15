7df72aacfdc46ad8f2b8b212ec7319327e9a2ff8
ghe <lili@snyk.io>
2021-06-15 18:10:25 +0100

    feat: use provided python intpreter
    When `snyk fix` or `snyk test` is called with --command
    user intends to provide a Python interperer to be used
    
    Consume this as a `python` config parameter and switch env
    with poetry when interpreter is provided and execute commands
    only after that.
    
    Switch back once `poetry add` command complets

--- packages/poetry/src/poetry-add.ts
+++ packages/poetry/src/poetry-add.ts
@@ -7,6 +7,7 @@ const debug = debugLib('snyk-fix:poetry');
 
 interface PoetryConfig {
   dev?: boolean;
+  python?: string; // python interpreter to use, cli used --command argument for this
 }
 
 const limiter = new Bottleneck({
@@ -26,6 +27,18 @@ async function runPoetryAdd(
     args.push('--dev');
   }
 
+  if (config.python) {
+    try {
+      // tell poetry to use the given interpreter
+      // https://python-poetry.org/docs/managing-environments/
+      await execute('poetry', ['env', 'use', config.python], {
+        cwd: projectPath,
+      });
+    } catch (e) {
+      debug(`'poetry use env ${config.python}' failed with`, e);
+    }
+  }
+
   try {
     res = await execute('poetry', args, {
       cwd: projectPath,
@@ -33,6 +46,17 @@ async function runPoetryAdd(
   } catch (e) {
     debug('Execute failed with', e);
     res = e;
+  }
+
+  if (config.python) {
+    try {
+      // set it back to system python
+      await execute('poetry', ['env', 'use', 'system'], {
+        cwd: projectPath,
+      });
+    } catch (e) {
+      debug(`'poetry use env system' failed with`, e);
+    }
   }
 
   return res;
--- packages/poetry/test/system/poetry-add.spec.ts
+++ packages/poetry/test/system/poetry-add.spec.ts
@@ -1,6 +1,13 @@
 import * as fs from 'fs';
 import * as pathLib from 'path';
+
+import * as snykChildProcess from '@snyk/child-process';
+
 import { poetryAdd } from '../../src';
+
+jest.mock('@snyk/child-process', () => ({
+  ...jest.requireActual('@snyk/child-process'),
+}));
 
 function backupFiles(root: string, files: string[]): void {
   for (const file of files) {
@@ -21,7 +28,10 @@ describe('poetryAdd', () => {
   const workspacesPath = pathLib.resolve(__dirname, 'workspaces');
   const OLD_ENV = process.env;
 
+  const poetryAddSpy = jest.spyOn(snykChildProcess, 'execute');
+
   afterEach(() => {
+    poetryAddSpy.mockReset();
     filesToDelete.map((f) => fs.unlinkSync(f));
   });
 
@@ -75,6 +85,7 @@ describe('poetryAdd', () => {
 
     // lockfile still has original version
     expect(fixedLockfileContent).toContain('1.11.0');
+    expect(poetryAddSpy).toBeCalledTimes(1);
 
     // restore original files
     restoreFiles(workspacesPath, [targetFile, lockFile]);
@@ -123,6 +134,7 @@ describe('poetryAdd', () => {
       'utf-8',
     );
 
+    expect(poetryAddSpy).toBeCalledTimes(1);
     expect(fixedLockfileContent).not.toContain('1.16.16');
 
     // restore original files
@@ -169,6 +181,57 @@ describe('poetryAdd', () => {
       pathLib.join(workspacesPath, lockFile),
       'utf-8',
     );
+    expect(poetryAddSpy).toBeCalledTimes(1);
+
+    // lockfile still has original version
+    expect(fixedLockfileContent).toContain('1.16.0');
+
+    // restore original files
+    restoreFiles(workspacesPath, [targetFile, lockFile]);
+    filesToDelete = [
+      pathLib.join(workspacesPath, 'simple/pyproject.toml.orig'),
+      pathLib.join(workspacesPath, 'simple/poetry.lock.orig'),
+    ];
+  }, 90000);
+
+  it('applies expected changes to pyproject.toml (100% success) with python2', async () => {
+    // Arrange
+    const targetFile = 'simple/pyproject.toml';
+    const expectedTargetFile = 'simple/expected-pyproject.toml';
+
+    const lockFile = 'simple/poetry.lock';
+    // backup original files
+    backupFiles(workspacesPath, [targetFile, lockFile]);
+    const packagesToInstall = ['six==1.16.0'];
+    // Act
+    const { dir } = pathLib.parse(pathLib.resolve(workspacesPath, targetFile));
+    const res = await poetryAdd(dir, packagesToInstall, { python: 'python2' });
+
+    // Assert
+    expect(res).toEqual({
+      command: 'poetry add six==1.16.0',
+      duration: expect.any(Number),
+      exitCode: 0,
+      stderr: '',
+      stdout: expect.stringContaining('Installing six'),
+    });
+    const fixedFileContent = fs.readFileSync(
+      pathLib.join(workspacesPath, targetFile),
+      'utf-8',
+    );
+    const expectedPyprojectContent = fs.readFileSync(
+      pathLib.join(workspacesPath, expectedTargetFile),
+      'utf-8',
+    );
+    expect(fixedFileContent).toEqual(expectedPyprojectContent);
+
+    // verify versions in lockfiles
+    const fixedLockfileContent = fs.readFileSync(
+      pathLib.join(workspacesPath, lockFile),
+      'utf-8',
+    );
+
+    expect(poetryAddSpy).toBeCalledTimes(3);
 
     // lockfile still has original version
     expect(fixedLockfileContent).toContain('1.16.0');
@@ -219,6 +282,7 @@ describe('poetryAdd', () => {
       'utf-8',
     );
 
+    expect(poetryAddSpy).toBeCalledTimes(1);
     // lockfile still has original version
     expect(fixedLockfileContent).toContain('dev');
     expect(fixedLockfileContent).toContain('0.1.22');
